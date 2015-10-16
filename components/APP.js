48437b2454112b82dd8ad96fb78aadac3ed8473f
gregkbarnes <gregkbarnes.mech@gmail.com>
2015-10-16 20:59:34 +0000

    added webpack and app-client

---
+++ components/APP.js
@@ -0,0 +1,11 @@
+var React = require("react");
+
+var APP = React.createClass({
+    render() {
+        return(
+            <h1>Hello from react</h1>
+        );
+    }
+});
+
+module.exports = APP;
